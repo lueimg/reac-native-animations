8778c3838e1ff4b9e393390ca54f0025ac04b1ca
luis <luis@luiss-MacBook-Pro.local>
2017-07-25 00:38:39 -0500

    animate rotation with rad

--- pages/Animation5.js
+++ pages/Animation5.js
@@ -39,7 +39,7 @@ class Animation5 extends Component {
     render() {
         const interpolateRotation = this.animatedValue.interpolate({
             inputRange: [0,1],
-            outputRange: ['0deg', '360deg']
+            outputRange: ['0rad', '10rad']
         })
 
         const animatedStyle = {
